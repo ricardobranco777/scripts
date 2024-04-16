a7e2851d5c241d67c440a39cca6cece8e4020593
Ricardo Branco <rbranco@suse.de>
2024-04-16 18:28:29 +0200

    Add gitupdate.sh

---
+++ gitupdate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+UPSTREAM="${UPSTREAM:-upstream}"
+
+update () {
+	branch=$(git branch --show-current)
+
+	if git remote | grep -qFx "$UPSTREAM" ; then
+		git fetch "$UPSTREAM" && git merge "$UPSTREAM/$branch"
+	else
+		git pull
+	fi
+}
+
+if [ $# -gt 1 ] ; then
+	for dir ; do
+		(cd "$dir" && update)
+	done
+else
+	update
+fi
