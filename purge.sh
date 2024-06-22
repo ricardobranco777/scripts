70218e0d128d75cee250d36eb91c51af9a68750b
Ricardo Branco <rbranco@suse.de>
2024-06-22 10:57:47 +0200

    Fix purge.sh

--- purge.sh
+++ purge.sh
@@ -2,4 +2,4 @@
 #
 # Purge packages in Debian/Ubuntu
 
-dpkg -l | awk '$1 == "rc" { print $2 ":" $4 }' | xargs -r sudo apt purge -y
+dpkg -l | awk '$1 == "rc" { print $2 }' | xargs -r sudo apt purge -y
