3b12d4284fb161b34e30e6eadfed6d12c90250fa
Ricardo Branco <rbranco@suse.de>
2026-01-30 14:05:10 +0100

    sysupgrade.sh: Use wget2 if available to fetch sets faster

--- sysupgrade.sh
+++ sysupgrade.sh
@@ -7,16 +7,21 @@
 set -e
 
 . /usr/pkg/etc/sysupgrade.conf
+ARCHIVE_EXTENSION="${ARCHIVE_EXTENSION:-tar.xz}"
 
 if [ -z "$RELEASEDIR" ] ; then
 	echo "ERROR: Needs RELEASEDIR set" >&2
 	exit 1
 fi
 
+get_sets() {
+	ls /etc/mtree | sed -n 's/^set\.//p'
+}
+
 check() {
 	# Get existing sets as a regular expression to be used to filter the hash list
 	# Otherwise sha512(1) will print ERROR on missing files
-	sets=$(ls /etc/mtree | sed -n 's/^set\.//p' | xargs echo | sed 's/ /|/g')
+	sets=$(get_sets | xargs echo | sed 's/ /|/g')
 
 	tmpfile=$(mktemp)
 	ftp -o - "$RELEASEDIR/binary/sets/SHA512" | grep -E "^SHA512 \(($sets)\.t" > "$tmpfile"
@@ -49,18 +54,29 @@ while [ $# -gt 0 ] ; do
 	esac
 done
 
-case "$cmd" in
+if [ "$cmd" = "auto" ] ; then
 	# Note: It makes no sense to run it on fetch as the mounted tmpfs will be gone
-	auto)
-		RELEASEDIR="${1:-$RELEASEDIR}" ;;
-	*)
-		exec sysupgrade $opts "$cmd" "$@" ;;
-esac
+	RELEASEDIR="${1:-$RELEASEDIR}"
+else
+	exec sysupgrade $opts "$cmd" "$@"
+fi
 
 size=$(ftp -o - "$RELEASEDIR/binary/sets/" | grep -Eo '[0-9]+kB' | awk '{ n += $1 } END { print n + 10 }')
 [ -z "$size" ] && exit 1
 
 mount_tmpfs -s "${size}k" tmpfs /var/cache/sysupgrade || exit 1
+
+if [ "$cmd" = "auto" ] ; then
+	# Use wget2 if available to fetch these sets faster
+	if command -v wget2 >/dev/null 2>&1; then
+		urls=""
+		for set in $(get_sets) ; do
+			urls="$urls $RELEASEDIR/binary/sets/$set.$ARCHIVE_EXTENSION"
+		done
+		sysupgrade clean
+		(cd /var/cache/sysupgrade && wget2 $urls)
+	fi
+fi
 
 cleanup() {
 	umount -v /var/cache/sysupgrade
